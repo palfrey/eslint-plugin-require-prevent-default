60be80d87142196431d68e5de6939f17ef3916ef
Tom Parker-Shemilt <palfrey@tevp.net>
2022-11-13 19:47:54 +0000

    Test js and ts parsers

--- tests/require-prevent-default.spec.js
+++ tests/require-prevent-default.spec.js
@@ -1,41 +1,65 @@
 const { RuleTester } = require("eslint");
 const requirePreventDefaultRule = require("../src/require-prevent-default.js");
-const ruleTester = new RuleTester({
-  parser: require.resolve("@babel/eslint-parser"),
-});
-ruleTester.run("require-prevent-default", requirePreventDefaultRule, {
-  valid: [
-    {
-      code: "<a onClick={(e) => {e.preventDefault();}}></a>",
-    },
-    {
-      code: "<a onClick={(event) => {event.preventDefault();}}></a>",
-    },
-    {
-      code: `<a onClick={(e) => {
+const ruleTesters = {
+  js: new RuleTester({
+    parser: require.resolve("@typescript-eslint/parser"),
+    parserOptions: { ecmaFeatures: { jsx: true } },
+  }),
+  ts: new RuleTester({
+    parser: require.resolve("@babel/eslint-parser"),
+  }),
+};
+Object.keys(ruleTesters).forEach((rt) => {
+  const ruleTester = ruleTesters[rt];
+  ruleTester.run(`require-prevent-default (${rt})`, requirePreventDefaultRule, {
+    valid: [
+      {
+        code: "<a onClick={(e) => {e.preventDefault();}}></a>",
+      },
+      {
+        code: "<a onClick={(event) => {event.preventDefault();}}></a>",
+      },
+      {
+        code: `<a onClick={(e) => {
         foo();
         e.preventDefault();
       }}></a>`,
-    },
-    {
-      code: "<button onClick={(e) => {e.preventDefault();}} />",
-    },
-    {
-      code: "<button onChange={() => {}} />",
-    },
-  ],
-  invalid: [
-    {
-      code: "<a onClick={(e) => {}}></a>",
-      errors: [{ messageId: "noPreventDefault" }],
-    },
-    {
-      code: "<a onClick={() => {}}></a>",
-      errors: [{ messageId: "noPreventDefaultArgs" }],
-    },
-    {
-      code: "<a onClick={(event) => {event.preventDefault;}}></a>",
-      errors: [{ messageId: "noPreventDefault" }],
-    },
-  ],
+      },
+      {
+        code: "<button onClick={(e) => {e.preventDefault();}} />",
+      },
+      {
+        code: "<button onChange={() => {}} />",
+      },
+      {
+        code: "function Foo() {return <a onClick={(e) => {e.preventDefault();}}></a>}",
+      },
+    ],
+    invalid: [
+      {
+        code: "<a onClick={(e) => {}}></a>",
+        errors: [{ messageId: "noPreventDefault" }],
+      },
+      {
+        code: "<a onClick={() => {}}></a>",
+        errors: [{ messageId: "noPreventDefaultArgs" }],
+      },
+      {
+        code: "<a onClick={(event) => {event.preventDefault;}}></a>",
+        errors: [{ messageId: "noPreventDefault" }],
+      },
+      {
+        code: "<div><a onClick={(e) => {}}></a></div>",
+        errors: [{ messageId: "noPreventDefault" }],
+      },
+      {
+        code: "function Foo() {return <a onClick={(e) => {}}></a>}",
+        errors: [{ messageId: "noPreventDefault" }],
+      },
+      {
+        code: "function Foo() {var x=false; return <div>{x && <a onClick={(e) => {}}></a>}</div>; }",
+        errors: [{ messageId: "noPreventDefault" }],
+      },
+    ],
+  });
 });
