fd52e8f0fdd5e4a149013a2eb0976a760e260e37
Tom Parker-Shemilt <palfrey@tevp.net>
2022-11-13 16:40:56 +0000

    Functioning check

--- src/require-prevent-default.js
+++ src/require-prevent-default.js
@@ -1,10 +1,46 @@
-module.exports = {
-    meta: {
-        messages: {
-            noPreventDefault: 'Require preventDefault in on* methods',
-        },
-    },
-    create(context) {
-        return {}
+function checkFunction(context, func) {
+  if (func.params.length == 0) {
+    context.report({ node: func.body, messageId: "noPreventDefaultArgs" });
+    return;
+  }
+  const eventArg = func.params[0].name;
+  for (const statement of func.body.body) {
+    if (
+      statement.type == "ExpressionStatement" &&
+      statement.expression.type == "CallExpression"
+    ) {
+      const callee = statement.expression.callee;
+      if (
+        callee.type == "MemberExpression" &&
+        callee.object.type == "Identifier" &&
+        callee.object.name == eventArg &&
+        callee.property.type == "Identifier" &&
+        callee.property.name == "preventDefault"
+      ) {
+        return;
+      }
     }
+  }
+  context.report({ node: func.body, messageId: "noPreventDefault" });
+}
+
+module.exports = {
+  meta: {
+    messages: {
+      noPreventDefault: "Require event.preventDefault in on* methods",
+      noPreventDefaultArgs:
+        "Require preventDefault in on* methods (missing event arg)",
+    },
+  },
+  create(context) {
+    return {
+      JSXOpeningElement(node) {
+        for (const attr of node.attributes) {
+          if (attr.name.name.startsWith("on")) {
+            checkFunction(context, attr.value.expression);
+          }
+        }
+      },
+    };
+  },
 };
--- tests/require-prevent-default.spec.js
+++ tests/require-prevent-default.spec.js
@@ -1,18 +1,22 @@
-const { RuleTester } = require('eslint');
-const requirePreventDefaultRule = require('../src/require-prevent-default.js');
+const { RuleTester } = require("eslint");
+const requirePreventDefaultRule = require("../src/require-prevent-default.js");
 const ruleTester = new RuleTester({
-  "parser": require.resolve("@babel/eslint-parser")
+  parser: require.resolve("@babel/eslint-parser"),
 });
-ruleTester.run('require-prevent-default', requirePreventDefaultRule, {
-    valid: [{
-        code: '<a onClick={(e) => {e.preventDefault();}}></a>'
-    }],
-    invalid: [{
-        code: '<a onClick={(e) => {}}></a>',
-        errors: [{ messageId: 'noPreventDefault' }],
-    },{
-        code: '<a onClick={() => {     }}></a>',
-        errors: [{ messageId: 'noPreventDefault' }],
-    }
-]
+ruleTester.run("require-prevent-default", requirePreventDefaultRule, {
+  valid: [
+    {
+      code: "<a onClick={(e) => {e.preventDefault();}}></a>",
+    },
+  ],
+  invalid: [
+    {
+      code: "<a onClick={(e) => {}}></a>",
+      errors: [{ messageId: "noPreventDefault" }],
+    },
+    {
+      code: "<a onClick={() => {}}></a>",
+      errors: [{ messageId: "noPreventDefaultArgs" }],
+    },
+  ],
 });
