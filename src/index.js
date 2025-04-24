37d0e5490f4580190c62de2146a9fad385f752a0
Tom Parker-Shemilt <palfrey@tevp.net>
2025-04-24 23:20:13 +0100

    0.1.3

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 module.exports = {
   meta: {
     name: "eslint-plugin-require-prevent-default",
-    version: "0.1.2",
+    version: "0.1.3",
   },
   rules: {
     "require-prevent-default": require("./require-prevent-default"),
